17d6048753f5de78afe737251f10b74e9c18e058
Jan-Amixtlan <janethamixtlan127@gmail.com>
2025-08-21 12:47:10 -0600

    feat: add routing with react-router-dom and create Home and About components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
 
 function App() {
   return (
-    <h1>Hola Mundo</h1>
+    <BrowserRouter>
+      <nav style={{ marginBottom: '2rem' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
