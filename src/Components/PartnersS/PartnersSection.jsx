11128fd84f94a81f196fb8cb6ef6ad13145fc1c0
Jan-Amixtlan <janethamixtlan127@gmail.com>
2025-08-21 17:07:17 -0600

    feat: update image paths in PartnersSection to use absolute URLs

--- src/Components/PartnersS/PartnersSection.jsx
+++ src/Components/PartnersS/PartnersSection.jsx
@@ -6,27 +6,27 @@ const PartnersSection = () => {
         {
             id: 1,
             name: "Partner 1",
-            img: require("../assets/logosPartener/img-partner-1.png")
+            img: "/src/assets/logosPartener/img-partner-1.png"
         },
         {
             id: 2,
             name: "Partner 2",
-            img: require("../assets/logosPartener/img-partner-2.png")
+            img: "/src/assets/logosPartener/img-partner-2.png"
         },
         {
             id: 3,
             name: "Partner 3",
-            img: require("../assets/logosPartener/img-partner-3.png")
+            img: "/src/assets/logosPartener/img-partner-3.png"
         },
         {
             id: 4,
             name: "Partner 4",
-            img: require("../assets/logosPartener/img-partner-4.png")
+            img: "/src/assets/logosPartener/img-partner-4.png"
         },
         {
             id: 5,
             name: "Partner 5",
-            img: require("../assets/logosPartener/img-partner-5.png")
+            img: "/src/assets/logosPartener/img-partner-5.png"
         }
     ];
 
