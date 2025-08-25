14e0168ee28e2d36b9be30f74411ba2497c2acb2
Jan-Amixtlan <janethamixtlan127@gmail.com>
2025-08-25 11:32:59 -0600

    feat: Add AllServicesBanner component with styling and responsive design

--- src/pages/ServicesGrid.jsx
+++ src/pages/ServicesGrid.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import Footer from '../Components/Footer/Footer';
 import NewsletterSubscription from '../Components/NewsLetterS/NewsletterSubscription.jsx';
+import AllServicesBanner from '../Components/AllServicesBanner/AllServicesBanner.jsx';
 
 const ServicesGrid = () => (
   <>
     <Navbar />
-    
+    <AllServicesBanner /> 
     <div style={{padding: '2rem'}}>
       <h1>Services Grid</h1>
       <p>Pantalla de grid de servicios.</p>
