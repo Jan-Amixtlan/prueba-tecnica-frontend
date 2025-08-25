e5e516decbabf8256f4f742ec7b76477df02fe71
Jan-Amixtlan <janethamixtlan127@gmail.com>
2025-08-25 17:25:23 -0600

    feat: Implement AutoServicesGrid component with service cards and navigation

--- src/pages/ServicesGrid.jsx
+++ src/pages/ServicesGrid.jsx
@@ -3,15 +3,13 @@ import Navbar from '../Components/Navbar/Navbar';
 import Footer from '../Components/Footer/Footer';
 import NewsletterSubscription from '../Components/NewsLetterS/NewsletterSubscription.jsx';
 import AllServicesBanner from '../Components/AllServicesBanner/AllServicesBanner.jsx';
+import AutoServicesGrid from '../Components/AutoServGrid/AutoServicesGrid.jsx';
 
 const ServicesGrid = () => (
   <>
     <Navbar />
     <AllServicesBanner /> 
-    <div style={{padding: '2rem'}}>
-      <h1>Services Grid</h1>
-      <p>Pantalla de grid de servicios.</p>
-    </div>
+    <AutoServicesGrid />      
     <NewsletterSubscription />
     <Footer />
   </>
