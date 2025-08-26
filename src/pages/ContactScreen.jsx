5326ad8022a4164b241dc24587b2d71032e4a165
Jan-Amixtlan <janethamixtlan127@gmail.com>
2025-08-25 20:33:18 -0600

    feat: Add ContactForm component with styling and functionality

--- src/pages/ContactScreen.jsx
+++ src/pages/ContactScreen.jsx
@@ -3,12 +3,14 @@ import Footer from "../Components/Footer/Footer"
 import NewsletterSubscription from "../Components/NewsLetterS/NewsletterSubscription";
 import PartnersSection from "../Components/PartnersS/PartnersSection";
 import ContactUsBanner from "../Components/ContactUsBanner/ContactUsBanner";
+import ContactForm from "../Components/ContactForm/ContactForm";
 
 const ContactScreen = () => {
   return (
     <>
       <Navbar />
       <ContactUsBanner />
+      <ContactForm />
       
       <PartnersSection />
       <NewsletterSubscription />
