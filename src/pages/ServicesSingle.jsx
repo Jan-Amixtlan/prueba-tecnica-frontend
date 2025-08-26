6848253f4fb4d26215146e0869177fc280014e51
Jan-Amixtlan <janethamixtlan127@gmail.com>
2025-08-25 22:11:47 -0600

    Add review form and service reviews components with styling
    - Implemented AddReviewForm component for submitting reviews with validation and success message.
    - Created ServiceReviews component to display a list of reviews with reply functionality.
    - Added CSS styles for both components to enhance UI/UX.
    - Included responsive design and accessibility features in the styles.

--- src/pages/ServicesSingle.jsx
+++ src/pages/ServicesSingle.jsx
@@ -4,13 +4,17 @@ import Footer from '../Components/Footer/Footer';
 import NewsletterSubscription from '../Components/NewsLetterS/NewsletterSubscription';
 import ServiceSingleBanner from '../Components/ServiceSingleBanner/ServiceSingleBanner';
 import EngineMountArticle from '../Components/EngineMountArticle/EngineMountArticle';
+import ServiceReviews from '../Components/ServiceReviews/ServiceReviews';
+import AddReviewForm from '../Components/AddReviewFormContact/AddReviewForm';
 
 const ServicesSingle = () => (
   <>
     <Navbar />
     <ServiceSingleBanner />
     <EngineMountArticle />
-    <h1>Services Single</h1>
+    <ServiceReviews />
+    <AddReviewForm />
+    
     <NewsletterSubscription />
     <Footer />
   </>
